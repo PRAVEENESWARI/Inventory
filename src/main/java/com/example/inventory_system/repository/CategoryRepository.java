b5b410dcfdb88ab10ec413828d0325daabc68550
PRAVEENESWARI <praveenme811@gmail.com>
2024-11-27 15:18:20 +0530

    Category Repository

--- src/main/java/com/example/inventory_system/repository/CategoryRepository.java
+++ src/main/java/com/example/inventory_system/repository/CategoryRepository.java
@@ -1,10 +1,18 @@
 package com.example.inventory_system.repository;
 
 import com.example.inventory_system.model.CategoryEntity;
+import com.example.inventory_system.model.ProductEntity;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity,Integer> {
     void deleteByCategoryId(int categoryId);
+    @Query("SELECT DISTINCT c.categoryType FROM CategoryEntity c")
+    List<String> findDistinctCategoryTypes();
+
 }
